c0e8e34802ebae9ae6238d2e290cbc5f0a96e587
animeshnik <40216914+quant0x2@users.noreply.github.com>
2020-02-26 00:08:43 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+import ("fmt";"os";"log")
+
+func main(){
+	 var answer, file string
+	 fmt.Println("Enter encrypt or decrypt")
+	 fmt.Scan(&answer)
+	 fmt.Println("Enter file name")
+	 fmt.Scan(&file)
+	 crypt(answer, file)
+}
+
+// Encryption using XOR
+func crypt(answer string, fileIn string){
+	 var file, outFile *os.File
+	 var err error 
+	 switch answer {
+	    case "encrypt":
+	 	    file, err = os.Open(fileIn)
+	 	    errorChecker(err)
+	 	    outFile, _ = os.Create(fileIn + ".enc")
+	 	case "decrypt":
+	 	    file, err = os.Open(fileIn)
+	 	    errorChecker(err)
+	 	    outFile, _ = os.Create("decrypted.txt")
+	 	default:
+	 	    fmt.Println("Enter encrypt or decrypt") 
+	 }
+	 defer file.Close()
+	 defer outFile.Close()
+	 fileInfo, _ := file.Stat()
+	 buffer := make([]byte, fileInfo.Size())
+	 file.Read(buffer)
+	 var arr []byte
+	 for i, _ := range buffer {
+	 	arr = append(arr, buffer[i] ^ getKeys(len(buffer))[i])
+	 }
+	 outFile.Write(arr)
+}
+
+// Keys generator  
+func getKeys(size int) []byte {
+	var keys []byte
+	for i:=1; i <= size; i++ {
+		keys = append(keys, 2 * uint8(i))
+	}
+	return keys
+}
+
+func errorChecker(err error) {
+	 if err != nil {
+	 	log.Fatal(err)
+	 }
+}
