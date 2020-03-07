c4ef7439e330e984ed4088218483e3ac3995933d
animeshnik <40216914+quant0x2@users.noreply.github.com>
2020-03-07 22:17:03 +0200

    Update main.go
    Replaced fmt.Scan() with command line arguments

--- main.go
+++ main.go
@@ -2,47 +2,52 @@ package main
 import ("fmt";"os";"log")
 
 func main(){
-	 var answer, file string
-	 fmt.Println("Enter encrypt or decrypt")
-	 fmt.Scan(&answer)
-	 fmt.Println("Enter file name")
-	 fmt.Scan(&file)
-	 crypt(answer, file)
+	arr := []string{"--encrypt", "--decrypt"}
+	if !commandChecker(arr, 1) {
+       fmt.Println("Help:")
+       fmt.Println("  --encrypt filename")
+       fmt.Println("  --decrypt filename")
+	} else {
+		crypt()
+	}
+	/*
+    if (len(os.Args) < 2) || ((os.Args[1] != "--encrypt") || (os.Args[1] != "--decrypt")) {
+       fmt.Println("Help:")
+       fmt.Println("  --encrypt filename")
+       fmt.Println("  --decrypt filename")
+    } else {
+	   crypt()
+    }
+    */
 }
 
 // Encryption with XOR
-func crypt(answer string, fileIn string){
-	 var file, outFile *os.File
-	 var err error 
-	 switch answer {
-	    case "encrypt":
-	 	    file, err = os.Open(fileIn)
-	 	    errorChecker(err)
-	 	    outFile, _ = os.Create(fileIn + ".enc")
-	 	case "decrypt":
-	 	    file, err = os.Open(fileIn)
-	 	    errorChecker(err)
+func crypt(){
+	 var outFile *os.File
+	 file, err := os.Open(os.Args[2])
+	 errorChecker(err)
+	 switch os.Args[1] {
+	    case "--encrypt":
+	 	    outFile, _ = os.Create(os.Args[2] + ".enc")
+	 	case "--decrypt":
 	 	    outFile, _ = os.Create("decrypted.txt")
-	 	default:
-	 	    fmt.Println("Enter encrypt or decrypt") 
 	 }
-	 defer file.Close()
-	 defer outFile.Close()
 	 fileInfo, _ := file.Stat()
 	 buffer := make([]byte, fileInfo.Size())
 	 file.Read(buffer)
-	 var arr []byte
 	 for i, _ := range buffer {
-	 	arr = append(arr, buffer[i] ^ getKeys(len(buffer))[i])
+	     buffer[i] ^= getKeys(len(buffer))[i]
 	 }
-	 outFile.Write(arr)
+	 outFile.Write(buffer)
+	 file.Close()
+	 outFile.Close()
 }
 
-// Keys generator  
+// Simple key generator for every symbol
 func getKeys(size int) []byte {
 	var keys []byte
-	for i:=1; i <= size; i++ {
-		keys = append(keys, 2 * uint8(i))
+	for i:=0; i < size; i++ {
+		keys = append(keys, uint8(i) / 2)
 	}
 	return keys
 }
@@ -51,4 +56,35 @@ func errorChecker(err error) {
 	 if err != nil {
 	 	log.Fatal(err)
 	 }
+}                                                
+// Pack of conditions for checking command        
+func commandChecker(criteria[]string, size int) bool{
+	 if len(os.Args) < 2 {
+	 	return false
+	 }
+	 var count int
+	 for i, _ := range os.Args {
+	 	if runeChecker(os.Args[i], '-') == 2 {
+	 	   for n, _ := range criteria { 
+	 	   	   if os.Args[i] == criteria[n] { 
+	 	   	   	     count++               
+	 	   	   }
+	 	   }
+	 	} 
+	 }
+	 if count == size{ 
+	 	return true
+   	 } else {
+   	 	return false
+   	 }
+}
+
+func runeChecker(str string, char rune) int {
+	var sum int
+	 	for j, _ := range []rune(str) { 
+	 	    if []rune(str)[j] == char { 
+	 	    	sum++
+	 	    }	
+	 	}
+	return sum
 }
